dc28d6bcfe952b48471a06e96f53b9af4f10bd81
Sindu Prasanna <chinthakayalasindu@gmail.com>
2024-10-04 09:01:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let menuIcon = document.querySelector(".menu-icon");
+let sidebar = document.querySelector(".sidebar");
+let container = document.querySelector(".container");
+
+menuIcon.addEventListener("click", function() {
+    sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");
+});
